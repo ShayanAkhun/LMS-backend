68787f3e41475fa62415774d351512f5f3a22e3e
ShayanAkhun <76954640+ShayanAkhun@users.noreply.github.com>
2024-01-17 17:03:40 +0500

    added course model

---
+++ models/course.model.ts
@@ -0,0 +1,29 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+interface IComment extends Document {
+    user: object,
+    comment: string
+}
+
+interface IReview extends Document {
+    user: object,
+    rating: string,
+    commentReplies: IComment[]
+}
+
+interface ILink extends Document {
+    title: string,
+    url: string
+}
+
+interface ICourseData extends Document {
+    title: string,
+    description: string,
+    videoUrl: string,
+    videoThumbnail: object,
+    videoLength: number,
+    videoPlayer: string,
+    links: ILink[],
+    suggestions: string,
+    questions: IComment[],
+}
