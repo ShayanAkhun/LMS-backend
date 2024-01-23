a7e035d8e5049adec3d02315de07308809061f0e
ShayanAkhun <76954640+ShayanAkhun@users.noreply.github.com>
2024-01-23 05:22:13 +0500

    added add answers and reply answers logic and fxed routes for courses

--- controllers/coruse.controller.ts
+++ controllers/coruse.controller.ts
@@ -6,6 +6,9 @@ import { createCourse } from "../services/course.service";
 import CourseModel from "../models/course.model";
 import { redis } from "../utils/redis";
 import mongoose from "mongoose";
+import ejs from 'ejs';
+import path from "path";
+import sendMail from "../utils/sendMail";
 
 
 
@@ -174,8 +177,101 @@ export const addQuestion = CatchAsyncErrors(async (req: Request, res: Response, 
             return next(new ErrorHandler("Invalid content Id", 400))
         }
 
-        const courseContent = course?.courseData?.find((item:any) =>item._id.equals(courseId))
-        
+        const courseContent = course?.courseData?.find((item: any) => item._id.equals(courseId))
+
+        if (!courseContent) {
+            return next(new ErrorHandler("Invalid content Id", 400))
+        }
+
+        //CREATE A NEW QUESTION
+        const newQuestion: any = {
+            user: req.user,
+            question,
+            questionReplies: []
+        }
+
+        //ADD THIS QUESTION TO OUR COURSE CONTENT
+        courseContent.questions.push(newQuestion)
+
+        //SAVE THIS UPDATE QUESTION
+        await course?.save()
+
+        res.status(200).json({
+            success: true,
+            course
+        })
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500))
+    }
+})
+
+
+//ANSWER THE QUESTION
+
+interface IAnswerQuestion {
+    answer: string,
+    courseId: string,
+    contentId: string,
+    questionId: string,
+}
+
+export const addAnswer = CatchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        const { answer, courseId, contentId, questionId }: IAnswerQuestion = req.body;
+        const course = await CourseModel.findById(courseId)
+
+        if (!mongoose.Types.ObjectId.isValid(contentId)) {
+            return next(new ErrorHandler("Invalid content Id", 400))
+        }
+
+        const courseContent = course?.courseData?.find((item: any) => item._id.equals(courseId))
+
+        if (!courseContent) {
+            return next(new ErrorHandler("Invalid content Id", 400))
+        }
+        const question = courseContent?.questions?.find((item: any) => item._id.equals(questionId))
+
+        if (!question) {
+            return next(new ErrorHandler("Invalid content Id", 400))
+        }
+
+        //CREATE A NEW ANSWER
+        const newAnswer: any = {
+            user: req.user,
+            answer
+        }
+
+        //ADD THIS QUESTION TO OUR COURSE CONTENT
+        question.questionReplies.push(newAnswer)
+
+        await course?.save()
+
+        if (req.user?._id === question.user._id) {
+            //CREATE A NOTIFICATION WHICH IS NOT CREATE YET
+        } else {
+            const data = {
+                name: question.user.name,
+                title: courseContent.title
+            }
+            const html = await ejs.renderFile(path.join(__dirname, "../mails/question-reply.js"), data);
+
+            try {
+                await sendMail({
+                    email: question.user.email,
+                    subject: "Question Reply",
+                    template: "question-reply.ejs",
+                    data
+                })
+            } catch (error: any) {
+                return next(new ErrorHandler(error.message, 500))
+            }
+        }
+        res.status(200).json({
+            success:true,
+            course
+        })
 
     } catch (error: any) {
         return next(new ErrorHandler(error.message, 500))
--- routes/course.routes.ts
+++ routes/course.routes.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import { authrorizeRoles, isAuthenticated } from '../middleware/auth';
-import { editCourse, uploadCourse, getSingleCourse, getAllCourse, getCourseByPremiumUser } from '../controllers/coruse.controller';
+import { editCourse, uploadCourse, getSingleCourse, getAllCourse, getCourseByPremiumUser, addQuestion, addAnswer } from '../controllers/coruse.controller';
 const courseRouter = express.Router()
 
 
 courseRouter.post("/create-course", isAuthenticated, authrorizeRoles('admin'), uploadCourse);
-courseRouter.post("/edit-course/:id", isAuthenticated, authrorizeRoles('admin'), editCourse);
-courseRouter.post("/get-course/:id", getSingleCourse);
-courseRouter.post("/get-courses", getAllCourse);
-courseRouter.post("/get-courses-content/:id",isAuthenticated, getCourseByPremiumUser);
+courseRouter.put("/edit-course/:id", isAuthenticated, authrorizeRoles('admin'), editCourse);
+courseRouter.get("/get-course/:id", getSingleCourse);
+courseRouter.get("/get-courses", getAllCourse);
+courseRouter.get("/get-courses-content/:id",isAuthenticated, getCourseByPremiumUser);
+courseRouter.put("/add-question",isAuthenticated, addQuestion);
+courseRouter.put("/add-answer",isAuthenticated, addAnswer);
 
 export default courseRouter;
