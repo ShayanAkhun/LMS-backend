b8465c497480de04b5217a6769bde6b8efe9803b
kashan ghori <64289221+khanakhun@users.noreply.github.com>
2025-07-21 13:57:16 +0500

    error handling

---
+++ utils/ErrorHandler.ts
@@ -0,0 +1,11 @@
+class ErrorHandler extends Error {
+    statusCode: Number;
+    constructor(message: any, statusCode: Number) {
+        super(message);
+        this.statusCode = statusCode;
+        
+        ErrorHandler.captureStackTrace(this,this.constructor)
+    }
+}
+export default ErrorHandler;
+
--- utils/db.ts
+++ utils/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 require("dotenv").config();
 
-const dbUrl: string = process.env.DB_URI || "";
+const dbUrl: string = process.env.DB_URL || "";
 
 const connectDB  = async () => {
     try {
@@ -9,9 +9,11 @@ const connectDB  = async () => {
         await mongoose.connect(dbUrl).then ((data:any) => {
             console.log(`Database connected with ${data.connection.host}`)  
         })
-    } catch (error) {
+    } catch (error :any) {
         console.log(error.message)
         setTimeout(connectDB, 5000)
     }
 }
+
+export default connectDB;
     
