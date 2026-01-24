5cece524a6b14e51c20852fc148a6c3f12da2256
ShayanAkhun <littelmaster391@gmail.com>
2026-01-24 19:44:48 +0500

    edit course changes

--- controllers/course-controller.ts
+++ controllers/course-controller.ts
@@ -4,6 +4,7 @@ import { Response, Request, NextFunction } from "express";
 import cloudinary from "cloudinary";
 import { CatchAsyncErrors } from "../middleware/CatchAsyncErrors";
 import { createCourse } from "../services/course.services";
+import { CourseModel } from "../models/course.model";
 
 
 
@@ -36,7 +37,7 @@ export const editCourse = CatchAsyncErrors(async (req: Request, res: Response, n
         const data = req.body;
         const thumbnail = data.thumbnail
 
-        if (thumbnail) {
+        if (thumbnail  && typeof thumbnail === "string") {
             await cloudinary.v2.uploader.destroy(data.public_id);
             const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
                 folder: "courses",
@@ -46,6 +47,18 @@ export const editCourse = CatchAsyncErrors(async (req: Request, res: Response, n
                 url: myCloud.secure_url
             }
         }
+        const courseId = req.params.id;
+
+        const course = await CourseModel.findByIdAndUpdate(courseId, {
+            $set: data
+        }, { new: true });
+
+        res.status(200).json({
+            success: true,
+            message: "Course updated successfully",
+            course
+
+        })
 
     } catch (error: any) {
         return next(new ErrorHandler(error.message, 500));
--- routes/course-route.ts
+++ routes/course-route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { authorizeRoles, isAuthenticated } from '../middleware/auth';
-import { uploadCourse } from '../controllers/course-controller';
+import { editCourse, uploadCourse } from '../controllers/course-controller';
 
 const CourseRouter = express.Router();
 
 
 CourseRouter.post('/create-course', isAuthenticated, authorizeRoles("admin"), uploadCourse);
+CourseRouter.put('/edit-course/:id', isAuthenticated, authorizeRoles("admin"), editCourse);
 
 export default CourseRouter;
